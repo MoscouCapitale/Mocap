3fdc830d6c67f944e825c3f98f30f0925e96c25e
Nikita-Philippe <nikitaphilippedu44@gmail.com>
2024-04-18 21:46:04 +0200

    🐛 Fix undefined storage

--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -1,17 +1,25 @@
-import { AppStorage } from '@models/App.ts'
+import { AppStorage } from "@models/App.ts";
 
 const saveToLocalStorage = (key: string, value: string): void => {
+  try {
     return localStorage.setItem(key, JSON.stringify(value));
-}
+  } catch (_e) {
+    console.error("Error saving to local storage");
+  }
+};
 
 const getFromLocalStorage = (key: string): string | null => {
+  try {
     return JSON.parse(localStorage.getItem(key) || "null");
-}
+  } catch (_e) {
+    return null;
+  }
+};
 
 export const saveAppStorage = (appStorage: AppStorage): void => {
-    saveToLocalStorage("APP_STORE", appStorage as string);
-}
+  saveToLocalStorage("APP_STORE", appStorage as string);
+};
 
 export const getAppStorage = (): AppStorage | null => {
-    return getFromLocalStorage("APP_STORE") as AppStorage;
-}
+  return getFromLocalStorage("APP_STORE") as AppStorage | null;
+};
