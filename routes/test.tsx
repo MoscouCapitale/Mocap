6a25cfef3bec16043b35039b531a263feb88d980
Nikita-Philippe <nikitaphilippedu44@gmail.com>
2024-05-06 21:22:21 +0200

    this shit does not work

--- routes/test.tsx
+++ routes/test.tsx
@@ -1,10 +1,8 @@
-import { Toaster } from "@islands/UI/Toast/Toaster.tsx"
 import TestIsland from "@islands/test.tsx";
 
 export default function Test(){
   return (<div>
     <h1>Test page</h1>
     <TestIsland/>
-    <Toaster />
   </div>);
 };
