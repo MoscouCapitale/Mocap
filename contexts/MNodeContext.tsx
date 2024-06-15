62a22ea78ba79b0d9669981c90dc7ec36ab2c186
Nikita-Philippe <nikitaphilippedu44@gmail.com>
2024-06-15 20:29:21 +0200

    WIP

--- contexts/MNodeContext.tsx
+++ contexts/MNodeContext.tsx
@@ -7,12 +7,22 @@ import { getBaseUrl } from "@utils/pathHandler.ts";
 
 type MCViewBox = { x: number; y: number; scale: number };
 
+type rectCollideProps = {
+  id: string;
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+};
+
 type MNodeContextType = {
   MCFrame: Signal<RefObject<HTMLElement>>;
   setMCFrame: (ref: Ref<HTMLElement>) => void;
   viewBox: Signal<MCViewBox>;
   setViewBox: (viewBox: MCViewBox) => void;
   MCNodes: Signal<MNode[]>;
+  isOverlapping: (a: rectCollideProps) => boolean | MNode | undefined;
+  saveNode: (node: MNode) => void;
 };
 
 // const asyncGnal = function<T>(defaultValue: T, callback: () => Promise<T>): ReadonlySignal<T> {
@@ -48,12 +58,39 @@ export const MNodeProvider = ({ children }: { children: VNode }) => {
       );
   });
 
+  const saveNode = (node: MNode) => {
+    const nodeIndex = MCNodes.value.findIndex(n => n.id === node.id)
+    if (nodeIndex !== -1){
+      MCNodes.value[nodeIndex] = node
+    } else {
+      MCNodes.value = [...MCNodes.value, node]
+    }
+  }
+
+  const isOverlapping = (a: rectCollideProps) => {
+    console.log(a, MCNodes.value);
+    const res = MCNodes.value.find((n) => {
+      if (n.id === a.id) return false;
+      const b = {
+        x1: n.x,
+        y1: n.y,
+        x2: n.x + n.width,
+        y2: n.y + n.height,
+      };
+      return a.x1 < b.x2 && a.x2 > b.x1 && a.y1 < b.y2 && a.y2 > b.y1;
+    })
+    console.log(res);
+    return res;
+  }
+
   const value = {
     MCFrame: MCFrame,
     setMCFrame: setMCFrame,
     viewBox: viewBox,
     setViewBox: setViewBox,
-    MCNodes: computed(() => MCNodes.value)
+    MCNodes: computed(() => MCNodes.value),
+    isOverlapping: (a: rectCollideProps) => isOverlapping(a),
+    saveNode: saveNode,
   };
 
   return (
